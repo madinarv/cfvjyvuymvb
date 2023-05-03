7f46f42da8b02581e1505eada76007d3bcd485be
User <User@RHD-PROG-9N.test.local>
2023-05-03 18:06:21 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slideshow-slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+let prevBtn = document.querySelector(".prev");
+let nextBtn = document.querySelector(".next");
+
+prevBtn.addEventListener("click", () => {
+    plusSlides(-1);
+});
+
+nextBtn.addEventListener("click", () => {
+    plusSlides(1);
+});
